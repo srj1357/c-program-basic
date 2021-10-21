559228e0dab764c35c3665490effb249921a9c03
srj1357 <surajverma.0501@gmail.com>
2021-10-21 23:45:24 +0530

    vfg

--- si30.c
+++ si30.c
@@ -3,7 +3,13 @@ int main()
 {
 printf("FINDING THE SIMPLE INTREST USING C\n");
 int p,r,t;
-
+printf("enter the principal value=");
+scanf("%d",&p);
+printf("enter the rate value=");
+scanf("%d",&r);
+printf("enter the  time=");
+scanf("%d",&t);
+printf("the simpal intrest=%d",(p*r*t)/100);
 
     return 0;
 }
